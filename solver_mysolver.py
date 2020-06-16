522f17efc40e0968d781bab40adf550d766b7b2a
chikaokuda <okudachika@icloud.com>
2020-06-16 21:49:29 +0900

    mysolver ver1

--- solver_mysolver.py
+++ solver_mysolver.py
@@ -3,12 +3,35 @@
 import sys
 
 from common import print_tour, read_input
+import solver_greedy as g
+#question: i felt like it’s redundant to have the same function in different scripts, so
+#          i imported from solver_greedy but now i feel like it's paying cost for the importing
+#          so i wonder which is better
+
+
+def dist_list(cities, tour, N):
+    dist = [0 for _ in range (N)]
+    for i in range (N):
+        dist[i] = g.distance(cities[tour[i]],cities[tour[(i+1)%N]]) 
+    return dist
 
 
 def solve(cities):
-    # Build a trivial solution.
-    # Visit the cities in the order they appear in the input.
-    return list(range(len(cities)))
+    N = len(cities)
+    tour = g.solve(cities)
+    dist = dist_list(cities, tour, N)
+    for i in range (1,N):                  # take two segments and compare with the switched order
+        entry_dot = tour[i]                # when switching reverse entry_dot ~ exit_dot
+        for j in range (i+1,N):
+            exit_dot = tour[j]
+            current_length = dist[i-1]+dist[j]
+            switched_length = g.distance(cities[tour[i-1]],cities[tour[j]])+g.distance(cities[tour[i]],cities[tour[(j+1)%N]])
+            if switched_length < current_length:
+                segment = tour[i:j+1]
+                segment.reverse()
+                tour[i:j+1] = segment
+
+    return tour
 
 
 if __name__ == '__main__':
