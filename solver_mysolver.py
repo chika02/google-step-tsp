23df0f6eec3b7f296a7eec450914fb0b422d33cd
chikaokuda <okudachika@icloud.com>
2020-06-18 12:49:15 +0900

    fixed mysolver

--- solver_mysolver.py
+++ solver_mysolver.py
@@ -11,41 +11,51 @@ def distance(city1, city2):
 def solve(cities):
     N = len(cities)
 
-    dist = [[0] * N for i in range(N)]
+    #store all the distance between nodes
+    dist = [[0] * (N+1) for i in range(N+1)]
     for i in range(N):
         for j in range(i, N):
             dist[i][j] = dist[j][i] = distance(cities[i], cities[j])
+    for i in range(N+1):
+        dist[N][i] = dist[0][i]
+        dist[i][N] = dist[i][0]
 
     current_city = 0
-    unvisited_cities = set(range(1, N))
+    unvisited_cities = list(range(1, N))
     tour = [current_city]
 
     while unvisited_cities:
-        next_city = min(unvisited_cities,
-                        key=lambda city: dist[current_city][city])
+
+        #append the next closest city to the tour list
+        next_city = min(unvisited_cities, key=lambda city: dist[current_city][city])
         unvisited_cities.remove(next_city)
         tour.append(next_city)
-        index = len(tour)-1
-        #print(index)
-        switch_index = 0
-        save_length = 0
-        if index > 2:
-            for i in range(index-3):
-                length = dist[tour[i]][tour[i+1]] + dist[tour[index-1]][tour[index]]
-                swiched_length = dist[tour[i]][tour[index-1]] + dist[tour[i+1]][tour[index]]
-                if length - swiched_length > save_length:
-                    save_length = length - swiched_length
-                    switch_index  = i
-            segment = tour[switch_index+1:index-1]    #reverse segment so it would be the shortest
-            segment.reverse()
-            tour[switch_index+1:index-1] = segment
-        #print(tour)
-        current_city = next_city
 
+        #consider switching the new segment with the already drawn segment.
+        #compare with all the segments
+        #if you can get shorter distance by switching, reverse all the cities in between (switch_point+1 ~ new_city-1).
+        new_city = len(tour)-1
+        switch_point = -1
+        save_length = -1
+        if new_city > 2:
+            for i in range(new_city-2):
+                length = dist[tour[i]][tour[(i+1)]] + dist[tour[new_city-1]][tour[new_city]]
+                switched_length = dist[tour[i]][tour[new_city-1]] + dist[tour[i+1]][tour[new_city]]
+                if (length - switched_length) > save_length:
+                    save_length = length - switched_length
+                    switch_point  = i
+            if switch_point != -1:
+                segment = tour[switch_point+1:new_city]
+                segment.reverse()
+                tour[switch_point+1:new_city] = segment
+        current_city = next_city
+        if current_city == N-1:
+            unvisited_cities.append(N)
+    tour = tour[:N]
     return tour
 
 
 if __name__ == '__main__':
     assert len(sys.argv) > 1
     tour = solve(read_input(sys.argv[1]))
-    #print_tour(tour)
+    print_tour(tour)
